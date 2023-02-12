160846d29c2f9fddefe818717212b8a94fb5d21e
kapureka6174 <mitsu.n.0109@gmail.com>
2023-02-12 19:04:11 +0900

    chore: change tone.js to start when button is pressed

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,29 @@
 import Canvas from './Canvas'
+import * as Tone from 'tone'
+import { useState } from 'react'
 
 const App = (): JSX.Element => {
+  const [checked, setChecked] = useState(false)
   return (
     <div>
-      <Canvas />
+      {!checked ? (
+        <button
+          type="button"
+          onClick={() => {
+            Tone.start()
+              .then(value => {
+                setChecked(true)
+              })
+              .catch(err => {
+                console.log(err)
+              })
+          }}
+        >
+          スタート
+        </button>
+      ) : (
+        <Canvas />
+      )}
     </div>
   )
 }
